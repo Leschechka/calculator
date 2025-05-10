ad7044ef8bf7990444d498b08d70a9ad6d705a46
Alex <suslo2000@list.ru>
2025-05-11 02:49:37 +0300

    accessing buttons with their ids

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+function operate(num1, operator, num2) {
+	
+}
+
 function showResult() {
 	span.innerText = firstNumber
 }
+
+const nullBtn = document.getElementById('null');
+const oneBtn = document.getElementById('one')
+const twoBtn = document.getElementById('two')
+const threeBtn = document.getElementById('three')
+const fourBtn = document.getElementById('four')
+const fiveBtn = document.getElementById('five')
+const sixBtn = document.getElementById('six')
+const sevenBtn = document.getElementById('seven')
+const eightBtn = document.getElementById('eight')
+const nineBtn = document.getElementById('nine')
+const clearBtn = document.getElementById('clear')
+const divideBtn = document.getElementById('divide')
+const multiplyBtn = document.getElementById('multiply')
+const minusBtn = document.getElementById('minus')
+const plusBtn = document.getElementById('plus')
+const equalBtn = document.getElementById('equal')
+equalBtn.addEventListener('click', operate)
 
 const span = document.querySelector('span');
 span.innerText = 0;
