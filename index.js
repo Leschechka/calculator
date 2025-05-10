44b5f133a8f9eed67e513be702e7af22f72666de
Alex <suslo2000@list.ru>
2025-05-11 02:37:46 +0300

    creating calculating logic

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function showResult() {
+	span.innerText = firstNumber
+}
+
+const span = document.querySelector('span');
+span.innerText = 0;
+
+let firstNumber = 0;
+let secondNumber;
+let operator;
