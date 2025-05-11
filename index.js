5ebc6ec706f325fc5a82f467f932d6475d345269
Alex <suslo2000@list.ru>
2025-05-11 16:39:57 +0300

    Thinking about the storing variables logic

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const add = (a, b) => a + b;
-const substract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-function operate(num1, operator, num2) {
-	
-}
-
-function showResult() {
-	span.innerText = firstNumber
-}
-
-const nullBtn = document.getElementById('null');
+const nullBtn = document.getElementById('null')
 const oneBtn = document.getElementById('one')
 const twoBtn = document.getElementById('two')
 const threeBtn = document.getElementById('three')
@@ -21,17 +8,97 @@ const sixBtn = document.getElementById('six')
 const sevenBtn = document.getElementById('seven')
 const eightBtn = document.getElementById('eight')
 const nineBtn = document.getElementById('nine')
-const clearBtn = document.getElementById('clear')
+
+
 const divideBtn = document.getElementById('divide')
 const multiplyBtn = document.getElementById('multiply')
 const minusBtn = document.getElementById('minus')
 const plusBtn = document.getElementById('plus')
+
+const clearBtn = document.getElementById('clear')
+clearBtn.addEventListener('click', clearScreen)
+
 const equalBtn = document.getElementById('equal')
 equalBtn.addEventListener('click', operate)
 
-const span = document.querySelector('span');
-span.innerText = 0;
+const add = (a, b) => a + b
+const substract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
 
-let firstNumber = 0;
-let secondNumber;
-let operator;
+const screen = document.querySelector('span');
+
+function checkIfHasOperator() {
+  if (
+    screen.innerText.includes('+') ||
+    screen.innerText.includes('-') ||
+    screen.innerText.includes('*') ||
+    screen.innerText.includes('÷')
+  ) {
+    return true
+  }
+  return false
+}
+
+const handleFirstNumber = (event) => {
+  if (
+    !checkIfHasOperator()
+  ) {
+    if (screen.innerText == '0') {
+      screen.innerText = event.target.innerText
+      firstNumber = event.target.innerText
+    } else {
+      screen.innerText += event.target.innerText
+      firstNumber += event.target.innerText
+    }
+  }
+}
+
+const handleOperator = (event) => {
+  if (!(screen.innerText[screen.innerText.length - 1] in operations)) {
+    screen.innerText += event.target.innerText;
+    operator = event.target.innerText
+  }
+}
+
+const handleSecondNumber = (event) => {
+  if (checkIfHasOperator) {
+    
+  }
+}
+
+const operators = document.querySelectorAll('.operator')
+for (let operator of operators) {
+  operator.addEventListener('click', handleOperator)
+}
+
+const operations = {
+  '+': add,
+  '-': substract,
+  '*': multiply,
+  '÷': divide,
+}
+
+function clearScreen() {
+  screen.innerText = 0
+}
+
+const digits = document.querySelectorAll(".digit");
+for (let digit of digits) {
+  digit.addEventListener('click', handleFirstNumber)
+}
+
+function operate(num1, operator, num2) {
+  // some operations
+}
+
+function showResult() {
+  screen.innerText = firstNumber
+}
+// object of buttons and their event listeners
+
+
+
+let firstNumber = 0
+let secondNumber
+let operator
