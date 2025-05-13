bebe9107ffc2ddbb611b5e6ea5a750cbf8139e64
Alex <suslo2000@list.ru>
2025-05-13 16:04:56 +0300

    adding event for digits which expect to add then their value to first number on the screen, creating function that launches this event as many times as it is called

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const substract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 
-const operand1Span = document.getElementById('operand1');
+const firstNumSpan = document.getElementById('operand1');
 const operatorSpan = document.getElementById('operator');
-const operand2Span = document.getElementById('operand2');
+const secondNumSpan = document.getElementById('operand2');
 
-let firstNumber = 0
-let secondNumber
-let operator
+const digitsEventFirstNum = (event) => {
+	if (firstNumSpan.innerText == '0') {
+		firstNumSpan.innerText = event.target.innerText
+	} else {
+		firstNumSpan.innerText += event.target.innerText
+	}
+}
+
+const addDigitsListenerForFirstNum = () => {
+	const digitBtns = document.querySelectorAll('.digit');
+	for (const digit of digitBtns) {
+		digit.addEventListener('click', digitsEventFirstNum)
+	}
+};
+
+addDigitsListenerForFirstNum()
